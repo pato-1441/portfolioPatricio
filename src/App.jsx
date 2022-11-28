75cb8921b91cc51aa51c46573176920d5e227f23
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-11-28 14:35:16 -0300

    first text added

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,10 @@ import MainText from "./components/MainText/MainText";
 
 const App = () => {
   return (
-    <div className="min-h-screen w-full bg-neutral-900 text-white">
-      <MainText/>
-      
+    <div className="min-h-screen w-full bg-neutral-900 text-white scroll-smooth">
+      <MainText />
     </div>
   );
-}
+};
 
 export default App;
--- src/components/MainText/MainText.jsx
+++ src/components/MainText/MainText.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 
 const MainText = () => {
-  return  <div className="p-20">
-            <h1 className="text-8xl">Patricio<br /><span>Albornoz</span></h1>
-            <h2 className="text-4xl mt-4">fullstack web developer</h2>
-          </div>;
+  return (
+    <div className="p-20 tracking-wider">
+      <h1 className="text-8xl">
+        Patricio
+        <br />
+        Albornoz
+      </h1>
+      <h2 className="text-4xl mt-4">fullstack web developer</h2>
+    </div>
+  );
 };
 
 export default MainText;
