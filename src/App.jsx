6de93587cd37a67eae711cf2dd1308aee848a3de
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2022-11-28 14:21:56 -0300

    empezando primera parte

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
+import MainText from "./components/MainText/MainText";
 
 const App = () => {
   return (
-    <div className="min-h-screen w-full bg-neutral-900">
-      
+    <div className="min-h-screen w-full bg-neutral-900 text-white">
+      <MainText/>
       
     </div>
   );
---
+++ src/components/MainText/MainText.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+const MainText = () => {
+  return  <div className="p-20">
+            <h1 className="text-8xl">Patricio<br /><span>Albornoz</span></h1>
+            <h2 className="text-4xl mt-4">fullstack web developer</h2>
+          </div>;
+};
+
+export default MainText;
