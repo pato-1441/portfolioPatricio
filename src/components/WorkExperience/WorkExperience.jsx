d72333be0d38127c86ac08148e087042f2b3fe5e
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-01-09 11:44:22 -0300

    icons working

--- src/components/WorkExperience/WorkExperience.jsx
+++ src/components/WorkExperience/WorkExperience.jsx
@@ -1,5 +1,5 @@
 import { Box, List, ListIcon, ListItem } from "@chakra-ui/react";
-import React from "react";
+import { MdCheckCircle, MdSettings } from "react-icons/all";
 
 const WorkExperience = () => {
   return (
@@ -7,23 +7,23 @@ const WorkExperience = () => {
       <h1 className="text-5xl">
         Work <br /> Experience
       </h1>
-      <Box fontSize={22} width={96}>
+      <Box mt={6} fontSize={22} width={96}>
         <List spacing={3}>
           <ListItem>
-            <ListIcon as={MdCheckCircle} color="green.500" />
+            <ListIcon as={MdCheckCircle} color="blue.300" />
             Lorem ipsum dolor sit amet, consectetur adipisicing elit
           </ListItem>
           <ListItem>
-            <ListIcon as={MdCheckCircle} color="green.500" />
+            <ListIcon as={MdCheckCircle} color="blue.300" />
             Assumenda, quia temporibus eveniet a libero incidunt suscipit
           </ListItem>
           <ListItem>
-            <ListIcon as={MdCheckCircle} color="green.500" />
+            <ListIcon as={MdCheckCircle} color="blue.300" />
             Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
           </ListItem>
           {/* You can also use custom icons from react-icons */}
           <ListItem>
-            <ListIcon as={MdSettings} color="green.500" />
+            <ListIcon as={MdSettings} color="blue.300" />
             Quidem, ipsam illum quis sed voluptatum quae eum fugit earum
           </ListItem>
         </List>
