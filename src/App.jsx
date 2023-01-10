5e19a0b6d83d2e74f50c4c3e8af420e7de7165f2
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-01-10 19:26:21 -0300

    adding second view of projects

--- src/App.jsx
+++ src/App.jsx
@@ -6,11 +6,13 @@ import WorkExperience from "./components/WorkExperience/WorkExperience";
 const App = () => {
   return (
     <div className="bg-neutral-900 text-white min-h-screen scroll-smooth">
-      <div className="container mx-auto flex justify-between pt-20">
+      <div className="container mx-auto flex justify-between pt-20 min-h-screen">
         <MainText />
         <WorkExperience />
-        <Projects />
         <DropMenu/>
+      </div>
+      <div className="container mx-auto flex justify-between pt-20 min-h-screen">
+        <Projects />
       </div>
     </div>
   );
