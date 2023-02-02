538374010a3175e0c136cd1a07ba7138d0ff6f3a
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-02-02 11:12:43 -0300

    chakra accordion added

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,53 +1,12 @@
 import { Box } from "@chakra-ui/react";
 import ProjectsCard from "../ProjectsCard/ProjectsCard";
 
-const myProjects = [
-  {
-    name: "Bornoz Bank",
-    image:
-      "https://user-images.githubusercontent.com/83987715/199247186-d0b2b54e-8275-4208-b85c-0aa3cb64dc96.jpeg",
-    link: "https://github.com/pato-1441/bornozBank",
-  },
-  {
-    name: "Bornoz Shows",
-    image:
-      "https://user-images.githubusercontent.com/83987715/200133740-f27eee0c-ca5b-4518-8966-8a9943be813a.jpeg",
-    link: "https://github.com/pato-1441/bornozShows",
-  },
-  {
-    name: "Bornoz Fly",
-    image:
-      "https://raw.githubusercontent.com/pato-1441/bornozFly/main/public/image-6.png",
-    link: "https://github.com/pato-1441/bornozFly",
-  },
-  {
-    name: "Marvel Wiki",
-    image:
-      "https://i.annihil.us/u/prod/marvel/images/OpenGraph-TW-1200x630.jpg",
-    link: "https://github.com/TimoteoGambella/Marvel-Project-Vercel",
-  },
-  {
-    name: "Search Synonyms",
-    image:
-      "https://user-images.githubusercontent.com/83987715/216335172-9f0f296d-b2e9-4f12-b55f-cde59b35ef4f.png",
-    link: "https://github.com/pato-1441/searchSynonyms",
-  },
-  {
-    name: "Old Portfolio",
-    image:
-      "https://user-images.githubusercontent.com/83987715/199248672-33f77843-bc2b-4ff1-8c10-7854e57df06d.png",
-    link: "https://github.com/pato-1441/portfolio",
-  },
-];
-
 const Projects = () => {
   return (
     <Box className="flex flex-col px-6">
       <h1 className="text-4xl">Projects</h1>
-      <div className="grid grid-cols-1 sm:grid-cols-3 gap-10 mt-6">
-        {myProjects.map((e) => (
-          <ProjectsCard name={e.name} image={e.image} link={e.link} />
-        ))}
+      <div className="grid grid-cols-1 mt-6">
+        <ProjectsCard />
       </div>
     </Box>
   );
--- src/components/ProjectsCard/ProjectsCard.jsx
+++ src/components/ProjectsCard/ProjectsCard.jsx
@@ -1,111 +1,109 @@
 import React, { useState } from "react";
 import { motion } from "framer-motion";
+import {
+  Accordion,
+  AccordionButton,
+  AccordionIcon,
+  AccordionItem,
+  AccordionPanel,
+  Box,
+} from "@chakra-ui/react";
 
-const ProjectsCard = ({ name, image, link }) => {
-  const itemVariants = {
-    open: {
-      opacity: 1,
-      y: 0,
-      transition: { type: "spring", stiffness: 300, damping: 24 },
-    },
-    closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
-  };
+const myProjects = [
+  {
+    id: 1,
+    name: "Bornoz Bank",
+    image:
+      "https://user-images.githubusercontent.com/83987715/199247186-d0b2b54e-8275-4208-b85c-0aa3cb64dc96.jpeg",
+    link: "https://github.com/pato-1441/bornozBank",
+  },
+  {
+    id: 2,
+    name: "Bornoz Shows",
+    image:
+      "https://user-images.githubusercontent.com/83987715/200133740-f27eee0c-ca5b-4518-8966-8a9943be813a.jpeg",
+    link: "https://github.com/pato-1441/bornozShows",
+  },
+  {
+    id: 3,
+    name: "Bornoz Fly",
+    image:
+      "https://raw.githubusercontent.com/pato-1441/bornozFly/main/public/image-6.png",
+    link: "https://github.com/pato-1441/bornozFly",
+  },
+  {
+    id: 4,
+    name: "Marvel Wiki",
+    image:
+      "https://i.annihil.us/u/prod/marvel/images/OpenGraph-TW-1200x630.jpg",
+    link: "https://github.com/TimoteoGambella/Marvel-Project-Vercel",
+  },
+  {
+    id: 5,
+    name: "Search Synonyms",
+    image:
+      "https://user-images.githubusercontent.com/83987715/216335172-9f0f296d-b2e9-4f12-b55f-cde59b35ef4f.png",
+    link: "https://github.com/pato-1441/searchSynonyms",
+  },
+  {
+    id: 6,
+    name: "Old Portfolio",
+    image:
+      "https://user-images.githubusercontent.com/83987715/199248672-33f77843-bc2b-4ff1-8c10-7854e57df06d.png",
+    link: "https://github.com/pato-1441/portfolio",
+  },
+];
 
-  const [isOpen, setIsOpen] = useState(false);
+const ProjectsCard = () => {
   return (
-    <motion.div
-      initial={false}
-      animate={isOpen ? "open" : "closed"}
-      className="my-5 rounded-xl"
-    >
-      <motion.button
-        className="text-xl flex items-center w-full justify-between border border-dashed hover:border-solid rounded-xl px-6 py-2"
-        whileTap={{ scale: 0.97 }}
-        onClick={() => setIsOpen(!isOpen)}
-        whileHover={{
-          boxShadow: "0px 0px 16px rgb(155,155,155)",
-        }}
-      >
-        {name}
-        <motion.div
-          variants={{
-            open: { rotate: 180, y: -2 },
-            closed: { rotate: 0 },
-          }}
-          transition={{ duration: 0.2 }}
-          style={{ originY: 0.55 }}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            strokeWidth={1.5}
-            stroke="currentColor"
-            className="w-6 h-6"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
-            />
-          </svg>
-        </motion.div>
-      </motion.button>
-      <motion.ul
-        variants={{
-          open: {
-            clipPath: "inset(0% 0% 0% 0% round 10px)",
-            transition: {
-              type: "spring",
-              bounce: 0,
-              duration: 0.7,
-              delayChildren: 0.3,
-              staggerChildren: 0.05,
-            },
-          },
-          closed: {
-            clipPath: "inset(10% 50% 90% 50% round 10px)",
-            transition: {
-              type: "spring",
-              bounce: 0,
-              duration: 0.3,
-            },
-          },
-        }}
-        style={{ pointerEvents: isOpen ? "auto" : "none" }}
-      >
-        <motion.li className="my-3 text-lg" variants={itemVariants}>
-          <motion.div className="h-fit">
-            <motion.div
-              className="bg-cover bg-center opacity-20 hover:opacity-100 rounded-b-xl"
-              style={{
-                backgroundImage: `url(${image})`,
-              }}
-            >
-              <p className="h-48"></p>
-            </motion.div>
-            <div className="text-md w-full flex flex-col sm:flex-row justify-between sm:gap-2">
-              <a
-                className="sm:w-1/2 text-center mt-4 py-2 rounded-xl border hover:bg-gray-500 transition-all duration-700"
-                href={link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <i className="fa-brands fa-github fa-xl"></i>
-              </a>
-              <a
-                className="sm:w-1/2 text-center mt-4 py-2 rounded-xl border hover:bg-cyan-900 transition-all duration-700"
-                href={link}
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                Deploy
-              </a>
-            </div>
-          </motion.div>
-        </motion.li>
-      </motion.ul>
-    </motion.div>
+    <div>
+      <motion.div className="my-5 rounded-xl">
+        <Accordion defaultIndex={[0]} allowMultiple>
+          {myProjects.map((e) => (
+            <AccordionItem key={e.id}>
+              <h2>
+                <AccordionButton>
+                  <Box as="span" flex="1" textAlign="left" fontSize={24}>
+                    {e.name}
+                  </Box>
+                  <AccordionIcon />
+                </AccordionButton>
+              </h2>
+              <AccordionPanel pb={4}>
+                <motion.div className="h-fit">
+                  <motion.div
+                    className="bg-cover bg-center opacity-20 hover:opacity-100 rounded-b-xl"
+                    style={{
+                      backgroundImage: `url(${e.image})`,
+                    }}
+                  >
+                    <p className="h-48"></p>
+                  </motion.div>
+                  <div className="text-md w-full flex flex-col sm:flex-row justify-between sm:gap-2">
+                    <a
+                      className="sm:w-1/2 text-center mt-4 py-2 rounded-xl border hover:bg-gray-500 transition-all duration-700"
+                      href={e.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      <i className="fa-brands fa-github fa-xl"></i>
+                    </a>
+                    <a
+                      className="sm:w-1/2 text-center mt-4 py-2 rounded-xl border hover:bg-cyan-900 transition-all duration-700"
+                      href={e.link}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      Deploy
+                    </a>
+                  </div>
+                </motion.div>
+              </AccordionPanel>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </motion.div>
+    </div>
   );
 };
 
