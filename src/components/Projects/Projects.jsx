9f2eeee3f8cb86e50e2feda5e83cae24e19c42bb
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-02-02 09:37:00 -0300

    seguimos con las cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -1,71 +1,11 @@
-import { Box, SimpleGrid } from "@chakra-ui/react";
-import { useState } from "react";
-import { motion } from "framer-motion";
+import { Box } from "@chakra-ui/react";
 import ProjectsCard from "../ProjectsCard/ProjectsCard";
 
 const Projects = () => {
-  const itemVariants = {
-    open: {
-      opacity: 1,
-      y: 0,
-      transition: { type: "spring", stiffness: 300, damping: 24 },
-    },
-    closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
-  };
-
-  const [isOpen, setIsOpen] = useState(false);
-
   return (
     <Box className="px-6">
       <h1 className="text-4xl">Projects</h1>
-      <SimpleGrid
-        mt={6}
-        spacing={10}
-        templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
-      >
-        <motion.div
-          className="menu flex flex-col col-span-1"
-          initial={false}
-          animate={isOpen ? "open" : "closed"}
-          whileInView={() => setIsOpen("open")}
-        >
-          {/* <div>ProjectsCard</div> */}
-          <motion.ul
-            variants={{
-              open: {
-                clipPath: "inset(0% 0% 0% 0% round 10px)",
-                transition: {
-                  type: "spring",
-                  bounce: 0,
-                  duration: 0.7,
-                  delayChildren: 0.3,
-                  staggerChildren: 0.05,
-                },
-              },
-              closed: {
-                clipPath: "inset(10% 50% 90% 50% round 10px)",
-                transition: {
-                  type: "spring",
-                  bounce: 0,
-                  duration: 0.3,
-                },
-              },
-            }}
-            style={{ pointerEvents: isOpen ? "auto" : "none" }}
-          >
-            <motion.li variants={itemVariants}>Item 1 </motion.li>
-            <motion.li variants={itemVariants}>Item 2 </motion.li>
-            <motion.li variants={itemVariants}>Item 3 </motion.li>
-            <motion.li variants={itemVariants}>Item 4 </motion.li>
-            <motion.li variants={itemVariants}>Item 5 </motion.li>
-          </motion.ul>
-          {/* <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-        sequi dolorum labore tenetur voluptas error, animi nobis sint
-        repudiandae doloremque.
-      </div> */}
-        </motion.div>
-      </SimpleGrid>
+      <ProjectsCard />
     </Box>
   );
 };
--- src/components/ProjectsCard/ProjectsCard.jsx
+++ src/components/ProjectsCard/ProjectsCard.jsx
@@ -1,15 +1,82 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "framer-motion";
 
 const ProjectsCard = () => {
+  const itemVariants = {
+    open: {
+      opacity: 1,
+      y: 0,
+      transition: { type: "spring", stiffness: 300, damping: 24 },
+    },
+    closed: { opacity: 0, y: 20, transition: { duration: 0.2 } },
+  };
+
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <motion.div className="flex flex-col col-span-1">
-      {/* <div>ProjectsCard</div> */}
-      {/* <div>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
-        sequi dolorum labore tenetur voluptas error, animi nobis sint
-        repudiandae doloremque.
-      </div> */}
+    <motion.div
+      initial={false}
+      animate={isOpen ? "open" : "closed"}
+      className="menu"
+    >
+      <motion.button
+        className="text-xl flex w-full justify-between"
+        whileTap={{ scale: 0.97 }}
+        onClick={() => setIsOpen(!isOpen)}
+      >
+        Menu
+        <motion.div
+          variants={{
+            open: { rotate: 180 },
+            closed: { rotate: 0 },
+          }}
+          transition={{ duration: 0.2 }}
+          style={{ originY: 0.55 }}
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-6 h-6"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M19.5 8.25l-7.5 7.5-7.5-7.5"
+            />
+          </svg>
+        </motion.div>
+      </motion.button>
+      <motion.ul
+        variants={{
+          open: {
+            clipPath: "inset(0% 0% 0% 0% round 10px)",
+            transition: {
+              type: "spring",
+              bounce: 0,
+              duration: 0.7,
+              delayChildren: 0.3,
+              staggerChildren: 0.05,
+            },
+          },
+          closed: {
+            clipPath: "inset(10% 50% 90% 50% round 10px)",
+            transition: {
+              type: "spring",
+              bounce: 0,
+              duration: 0.3,
+            },
+          },
+        }}
+        style={{ pointerEvents: isOpen ? "auto" : "none" }}
+      >
+        <motion.li variants={itemVariants}>Item 1 </motion.li>
+        <motion.li variants={itemVariants}>Item 2 </motion.li>
+        <motion.li variants={itemVariants}>Item 3 </motion.li>
+        <motion.li variants={itemVariants}>Item 4 </motion.li>
+        <motion.li variants={itemVariants}>Item 5 </motion.li>
+      </motion.ul>
     </motion.div>
   );
 };
