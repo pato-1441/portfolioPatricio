115138f874115434e2b7c96480acecc1817aa7fb
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-02-02 09:01:36 -0300

    starting projects section

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,34 +9,21 @@ import {
   SimpleGrid,
   Image,
 } from "@chakra-ui/react";
+import ProjectsCard from "../ProjectsCard/ProjectsCard";
 
 const Projects = () => {
   return (
-    <Box>
+    <Box className="px-6">
       <h1 className="text-4xl">Projects</h1>
       <SimpleGrid
         mt={6}
-        spacing={4}
+        spacing={10}
         templateColumns="repeat(auto-fill, minmax(300px, 1fr))"
       >
-        <Card color="white" bgColor="red.700">
-          <CardHeader>
-            <Text fontSize={22}> Customer dashboard</Text>
-          </CardHeader>
-          <Image
-            objectFit="cover"
-            src="https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80"
-            alt="Chakra UI"
-          />
-          <CardBody>
-            <Text>
-              View a summary of all your customers over the last month.
-            </Text>
-          </CardBody>
-          <CardFooter color="black">
-            <Button>View here</Button>
-          </CardFooter>
-        </Card>
+        <ProjectsCard />
+        <ProjectsCard />
+        <ProjectsCard />
+        <ProjectsCard />
       </SimpleGrid>
     </Box>
   );
---
+++ src/components/ProjectsCard/ProjectsCard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const ProjectsCard = () => {
+  return (
+    <div className="flex flex-col col-span-1">
+      <div>ProjectsCard</div>
+      <div>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam
+        sequi dolorum labore tenetur voluptas error, animi nobis sint
+        repudiandae doloremque.
+      </div>
+    </div>
+  );
+};
+
+export default ProjectsCard;
