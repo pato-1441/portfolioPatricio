ca7cc2ada4ad2798384cc06b3d8dd2c744b42c27
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-02-07 19:19:10 -0300

    adding the new section, and going to add smooth scroll-react

--- src/App.jsx
+++ src/App.jsx
@@ -3,9 +3,12 @@ import MainText from "./components/MainText/MainText";
 
 const App = () => {
   return (
-    <div className="bg-black text-white min-h-screen scroll-smooth">
+    <div className="bg-black text-white min-h-screen ">
       <div className="container mx-auto flex flex-col justify-between pt-10 sm:pt-20 min-h-screen">
         <MainText />
+      </div>
+      <div id="projectsSection" className="container mx-auto flex flex-col justify-between pt-10 sm:pt-20 min-h-screen">
+        <Projects />
       </div>
       <div className="container mx-auto flex flex-col justify-between pt-10 sm:pt-20 min-h-screen">
         <Projects />
--- src/components/DownArrow/DownArrow.jsx
+++ src/components/DownArrow/DownArrow.jsx
@@ -1,35 +1,37 @@
-import React from "react";
+import React, { Link } from "react";
 import { motion } from "framer-motion";
 
 const DownArrow = () => {
   return (
-    <motion.svg
-      className="w-16 h-16 sm:w-20 sm:h-20"
-      initial={{ opacity: 1 }}
-      animate={{ y: [-5, 5, -5] }}
-      transition={{
-        repeat: Infinity,
-        repeatType: "mirror",
-        type: "spring",
-        duration: 3,
-      }}
-      whileTap={{ scale: 0.9 }}
-    >
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        strokeWidth={1}
-        stroke="currentColor"
-        className="w-6 h-6"
+    <a href="#projectsSection">
+      <motion.svg
+        className="w-16 h-16 sm:w-20 sm:h-20"
+        initial={{ opacity: 1 }}
+        animate={{ y: [-5, 5, -5] }}
+        transition={{
+          repeat: Infinity,
+          repeatType: "mirror",
+          type: "spring",
+          duration: 3,
+        }}
+        whileTap={{ scale: 0.9 }}
       >
-        <path
-          strokeLinecap="round"
-          strokeLinejoin="round"
-          d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
-        />
-      </svg>
-    </motion.svg>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1}
+          stroke="currentColor"
+          className="w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M9 12.75l3 3m0 0l3-3m-3 3v-7.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+      </motion.svg>
+    </a>
   );
 };
 
