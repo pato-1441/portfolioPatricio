8abc309f787dead269985664a479b48b459af8bf
Patricio Albornoz <wagnerpatricio@outlook.com.ar>
2023-03-28 19:36:12 -0300

    text changed

--- src/components/MainText/MainText.jsx
+++ src/components/MainText/MainText.jsx
@@ -22,8 +22,7 @@ const MainText = () => {
         <div className="sm:w-3/4 col-span-2 sm:col-span-1 sm:h-96">
           <p>
             Web developer, trained in technologies such as C#, .NET, Javascript, React,
-            Express, Mongo, Git, among others. Constant desire to learn. Good
-            use of organization, time and communication. Looking for new
+            Express, Mongo, Git, among others. Constant desire to learn. Fan of organization, team management and making socials. Looking for new
             challenges that allow me to develop new solutions.
           </p>
           <br />
